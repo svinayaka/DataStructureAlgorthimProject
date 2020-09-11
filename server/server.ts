41ea50adb83dc2c6804eddb322ced4385e4f992e
siddhi vinayaka <svinayaka290489@gmail.com>
2020-09-11 18:30:40 +0530

    update linear search folder

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+
+
 app.get('/', (req, res, next):void => {
     res.send('Started running, True!');
 })
